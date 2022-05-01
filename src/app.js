69d1e63618ab7c5800a460026d922a7d30948500
AnnaBleakley <bleakleyanna@gmail.com>
2022-05-01 12:04:30 +0100

    Converted Celcius to Fahrenheit and vice-versa
    - added javascript to convert temperature
    - updated CSS and HTML to accommodate these changes

--- src/app.js
+++ src/app.js
@@ -1,56 +1,71 @@
-//Feature 1: updating dates
-let longDays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
 
-let now = new Date();
-console.log(now);
-let days = longDays[now.getDay()];
-let hour = now.getHours();
-let min = now.getMinutes();
-let timeDate = document.querySelector("#currentDate");
-timeDate.innerHTML = `${days} ${hour}:${min}`;
 
-//Feature 2: display name on page of user's value
-function cityAlert(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city");
-  let newCity = document.querySelector("#currentCity");
-  newCity.innerHTML = `${cityInput.value}`;
-  //alert(`${cityInput.value}`);
-}
 
-let cityInsert = document.querySelector("#signup-form");
-cityInsert.addEventListener("submit", cityAlert);
 
-//Get Current temperature in current city
-function showWeather(response) {
-  let currentTemp = document.querySelector("#celcius");
-  let temperature = Math.round(response.data.main.temp);
-  currentTemp.innerHTML = `${temperature}°`;
-}
 
-function showPosition(position) {
-  let apiKey = "ada02e10de1af69bc78fee31cecf36f4";
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
-  let url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
-  axios.get(url).then(showWeather);
-}
 
-navigator.geolocation.getCurrentPosition(showPosition);
 
-//fahrenheit converter
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  alert("Link clicked");
-}
 
-let fahrenheitLink = document.querySelector("#fahren");
-fahrenheitLink.addEventListener(click, "displayFahrenheitTemperature");
+
+
+
+
+    let longDays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let now = new Date();
+    console.log(now);
+    let days = longDays[now.getDay()];
+    let hour = now.getHours();
+    let min = now.getMinutes();
+    let timeDate = document.querySelector("#currentDate");
+    timeDate.innerHTML = `${days} ${hour}:${min}`;
+    let celsiusTemperature = response.data.main.temp;
+
+    //Feature 2: display name on page of user's value
+    function cityAlert(event) {
+      event.preventDefault();
+      let cityInput = document.querySelector("#city");
+      let newCity = document.querySelector("#city");
+      newCity.innerHTML = `${cityInput.value}`;
+    }
+
+    let cityInsert = document.querySelector("#signup-form");
+    cityInsert.addEventListener("submit", cityAlert);
+
+    //Get Current temperature in current city
+    function showWeather(response) {
+      let currentTemp = document.querySelector("#celcius");
+      let temperature = Math.round(response.data.main.temp);
+      currentTemp.innerHTML = `${temperature}°`;
+    }
+
+    function showPosition(position) {
+      let apiKey = "ada02e10de1af69bc78fee31cecf36f4";
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+      let url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+      axios.get(url).then(showWeather);
+    }
+
+    navigator.geolocation.getCurrentPosition(showPosition);
+
+    //fahrenheit converter
+    function displayFahrenheitTemperature(event) {
+      event.preventDefault();
+      let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+      let temperatureElement = document.querySelector("#temperature");
+      temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+    }
+
+    celsiusTemperature = null;
+
+    let fahrenheitLink = document.querySelector("#fahren");
+    fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
